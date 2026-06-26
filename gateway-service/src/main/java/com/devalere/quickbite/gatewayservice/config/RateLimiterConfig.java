32a027ddb29594b0e8a41fcec23e18d9a73f9bd3
devalere <devalerek@gmail.com>
2026-06-26 11:54:54 +0200

    feat(v2): configuration routage + rate limiting

---
+++ gateway-service/src/main/java/com/devalere/quickbite/gatewayservice/config/RateLimiterConfig.java
@@ -0,0 +1,28 @@
+package com.devalere.quickbite.gatewayservice.config;
+
+import java.util.Objects;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig
+{
+
+    /**
+     * Rate limiting par adresse IP du client.
+     * Chaque IP a son propre compteur dans Redis.
+     * <p>
+     * Alternatives possibles – Par user ID (extraire du JWT). Par API key (header custom)
+     */
+    @Bean
+    public KeyResolver ipKeyResolver(){
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest()
+                                .getRemoteAddress())
+                        .getAddress()
+                        .getHostAddress());
+    }
+}
